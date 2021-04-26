e28f11c45d3ce1b23ac238a32c29ec505b87dfc1
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-26 19:26:43 +0530

    calculate length of (x,y) co-ordinates

--- src/LineComparisonProblem.java
+++ src/LineComparisonProblem.java
@@ -1,9 +1,28 @@
-
+import java.util.*;
 public class LineComparisonProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparasion problem ");
-
+		Scanner s = new Scanner(System.in);
+		
+		int x1,x2,y1,y2;
+		System.out.println("enter details for line 1");
+		
+		x1=s.nextInt();
+		x2=s.nextInt();
+		y1=s.nextInt();
+		y2=s.nextInt();
+		 double len1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		 System.out.println("length of line 1 is " + len1);
+	
+		 System.out.println("enter details for line 2");
+			
+		x1=s.nextInt();
+		x2=s.nextInt();
+		y1=s.nextInt();
+		y2=s.nextInt();
+		 double len2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		 System.out.println("length of line 2 is " + len2);
 	}
 
 }
